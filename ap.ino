314a30ae9a20370db27b280e42b5213a5be52768
Alexander Andl <alexpost@andl.de>
2021-01-08 08:50:21 +0100

    - debouncing - rgb led

--- ap.ino
+++ ap.ino
@@ -1,18 +1,17 @@
-//#include <Keyboard.h>
 #include <Joystick.h>
 
-
 #define INTERNAL_LED   13   // on
-#define LED_RED        24   // on, when rotary is rotated 
-#define LED_BLUE       25   // on, when button is pressed
-#define LED_GREEN      26   // on, when red and blue is off
+#define LED_RED        33   // on, when rotary is rotated 
+#define LED_GREEN      34   // on, when red and blue is off
+#define LED_BLUE       35   // on, when button is pressed
 
-#define LED_WAIT       1000 // time in millis, so long is red andf blue on
+
+#define LED_WAIT       300 // time in millis, so long is red andf blue on
 
 // pins for the rotary encoders
-#define R1_1  14 // Interrupt
-#define R1_2  15
-
+#define R1_1  24 // Interrupt
+volatile unsigned int lr11 = 0;
+#define R1_2  25
 #define R2_1  16 // Interrupt
 #define R2_2  17
 
@@ -25,17 +24,16 @@
 #define BTNSIZE  10
 const int BTN[10] = {0,1,2,3,4,5,6,7,8,9};  // pins for the buttons
 const int BTNOUT[10] = {21,22,23,24,25,26}; // id for the joystick buttons
-int btnlast[10] = {0,0,0,0,0,0,0,0,0,0}; // last status of the buttons
+int btnlast[10] = {1,1,1,1,1,1,1,1,1,1}; // last status of the buttons
 
 #define RSIZE  4
 volatile int rval[4] = {0,0,0,0}; // open envents from rotary encoders 
-//const char charinc[4] = {'A','B','C','D'};
-//const char chardec[4] = {'a','b','c','d'};
 const int BTNINC[4] = {11,12,13,14}; // joystick buttons for increment
 const int BTNDEC[4] = {1,2,3,4}; // joystick buttons for decrement
 
 unsigned int mred = 0; // current millis when red led isturned on
 unsigned int mblue= 0; // current millis when blue led isturned on
+long v1 =0 ;
 
 void setup() {
   int i;
@@ -59,15 +57,22 @@ void setup() {
   pinMode(LED_BLUE, OUTPUT); 
   pinMode(LED_GREEN, OUTPUT);
   green();
+  digitalWrite(LED_RED,HIGH);
+  digitalWrite(LED_GREEN,HIGH);
+  digitalWrite(LED_BLUE,HIGH);  
+
 
   for (i=0;i<BTNSIZE;i++) {
     pinMode(BTN[i],INPUT); 
+    Joystick.button(BTNOUT[i],0);
   }
 
-  attachInterrupt(digitalPinToInterrupt(R1_1), do1, FALLING);
-  attachInterrupt(digitalPinToInterrupt(R2_1), do2, FALLING);
-  attachInterrupt(digitalPinToInterrupt(R3_1), do3, FALLING);
-  attachInterrupt(digitalPinToInterrupt(R4_1), do4, FALLING);
+  attachInterrupt(digitalPinToInterrupt(R1_1), do11, FALLING);
+  attachInterrupt(digitalPinToInterrupt(R1_2), do12, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(R1_2), do12, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(R2_1), do2, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(R3_1), do3, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(R4_1), do4, FALLING);
 
   //Keyboard.begin();
   Joystick.begin();
@@ -76,7 +81,7 @@ void setup() {
 void loop() {
   int i;
   int b;
-  
+
   for (i=0;i<RSIZE;i++) {
     sendEvent(i);
   }
@@ -89,7 +94,7 @@ void loop() {
         Serial.print(":");
         Serial.println(b);
 
-        Joystick.button(BTNOUT[i],b);
+        Joystick.button(BTNOUT[i],b==1?0:1);
         blue();
         btnlast[i] = b;
      }
@@ -100,6 +105,7 @@ void loop() {
 }
 
 void ledstatus() {
+  
   if (mred > 0) {
     if (millis()>mred+LED_WAIT) {      
       green();
@@ -117,54 +123,58 @@ void ledstatus() {
 }
 
 void sendEvent(int i) {
-    while (rval[i] != 0) {
+    if (rval[i] != 0) {   
+      int x = rval[i];   
+      rval[i]=0;
       Serial.print("ROE");
-      Serial.print(i);
-      if (rval[i] < 0) {
-        Joystick.button(i,BTNINC[i]);
-        //Keyboard.write(charinc[i]);        
-        rval[i]++;
+      Serial.print(i);      
+      if (x < 0) {        
+        Joystick.button(BTNINC[i],1);
+        delay(90);
+        Joystick.button(BTNINC[i],0);
         Serial.println("++");
-      } else {
-        Joystick.button(i,BTNDEC[i]);
-        //Keyboard.write(chardec[i]);        
-        rval[i]--;
+        blue();
+      } else if (x > 0) {
+        Joystick.button(BTNDEC[i],1);
+        delay(90);
+        Joystick.button(BTNDEC[i],0);
         Serial.println("--");
+        red();
       }      
-      red();
+      
+      
     }      
 }
 
-void do1() {
-  if (digitalRead(R1_2) == HIGH) {
-    rval[0] ++;    
-  } else {
-    rval[0] --;    
-  }  
+void do11() {
+  if (millis() < lr11 + 100)  return;
+  lr11 = millis();
+
+  if (digitalRead(R1_2) == HIGH) {    
+      rval[0]++;    
+  } 
+}
+
+
+void do12() {
+  if (millis() < lr11 + 100)  return;
+  lr11 = millis();
+
+  if (digitalRead(R1_1) == HIGH) {    
+      rval[0]--;    
+  } 
 }
 
 void do2() {
-  if (digitalRead(R2_2) == HIGH) {
-    rval[1] ++;    
-  } else {
-    rval[1] --;    
-  }  
+  
 }
 
 void do3() {
-  if (digitalRead(R3_2) == HIGH) {
-    rval[2] ++;
-  } else {
-    rval[2] --;    
-  }  
+
 }
 
 void do4() {
-  if (digitalRead(R4_2) == HIGH) {
-    rval[3] ++;    
-  } else {
-    rval[3] --;    
-  }  
+
 }
 
 
