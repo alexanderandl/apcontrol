6903038267d157f7d9676136e9705d84378542ab
Alexander Andl <alexpost@andl.de>
2021-01-13 15:14:35 +0100

    fertiges Layout

--- ap.ino
+++ ap.ino
@@ -10,21 +10,21 @@
 
 #define LED_WAIT       300 // time in millis, so long is red andf blue on
 #define INPUT_WAIT     20
-#define LOOP_DELAY     10
+#define LOOP_DELAY     20
 
 // pins for the rotary encoders
 #define R1_1  24 // BARO
 #define R1_2  25 
 #define R2_1  26 // CRS
 #define R2_2  27  
-#define R3_1  18 // HDG 
-#define R3_2  19
-#define R4_1  20 // SPD
-#define R4_2  21
-#define R5_1  20 // ALT
-#define R5_2  21
-#define R6_1  20 // VS
-#define R6_2  21
+#define R3_1  28 // HDG 
+#define R3_2  29
+#define R4_1  30 // SPD
+#define R4_2  31
+#define R5_1  14 // ALT
+#define R5_2  15
+#define R6_1  16 // VS
+#define R6_2  17
 
 
 
@@ -57,17 +57,18 @@ int btnlast[12] = {1,1,1,1,1,1,1,1,1,1,1,1}; // last status of the buttons
 
 #define RSIZE  6
 volatile int rval[6] = {0,0,0,0,0,0}; // open envents from rotary encoders 
+volatile int rvalr[6] = {0,0,0,0,0,0}; 
 const int BTNINC[6] = {11,12,13,14,15,16}; // joystick buttons for increment
 const int BTNDEC[6] = {1,2,3,4,5,6}; // joystick buttons for decrement
 
-unsigned int mled = 0; // current millis when led isturned on
+unsigned int mled = 0; // current millis when led is turned on
 
 long v1 =0 ;
 
 void setup() {
   int i;
 
-  if (DEBUG) {
+  if (DEBUG>0) {
     Serial.begin(115200);
     Serial.println("Starte....");
   }
@@ -129,7 +130,7 @@ void loop() {
   for (int i=0;i<BTNSIZE;i++) {
      int b = digitalRead(BTN[i]);
      if (b != btnlast[i]) {
-        if (DEBUG) {
+        if (DEBUG>0) {
           Serial.print("BTN");
           Serial.print(i);
           Serial.print(":");
@@ -145,7 +146,12 @@ void loop() {
   ledstatus();
 
   delay(LOOP_DELAY);
+  
+  for (int i=0;i<RSIZE;i++) {
+    resetEvent(i);
+  }
 }
+
 
 void ledstatus() {
   
@@ -161,142 +167,161 @@ void ledstatus() {
 void sendEvent(int i) {
     if (rval[i] != 0) {   
       int x = rval[i];   
-      rval[i]=0;
-      if (DEBUG) {
+      rval[i] = 0;
+      rvalr[i] = 1;
+      if (DEBUG>0) {
         Serial.print("ROE");
         Serial.print(i);      
       }
       if (x < 0) {        
         Joystick.button(BTNINC[i],1);
-        delay(90);
-        Joystick.button(BTNINC[i],0);
-        if (DEBUG) Serial.println("++");
+        if (DEBUG>0) Serial.println("++");
         blue();
       } else if (x > 0) {
         Joystick.button(BTNDEC[i],1);
-        delay(90);
-        Joystick.button(BTNDEC[i],0);
-        if (DEBUG) Serial.println("--");
+        if (DEBUG>0) Serial.println("--");
         red();
       }      
-      
-      
     }      
+}
+
+void  resetEvent(int i) {
+  if (rvalr[i] != 0)  {    
+    rvalr[i] = 0;
+    Joystick.button(BTNINC[i],0);
+  }
 }
 
 // 1
 void do11() {
-  if (millis() < lr11 + INPUT_WAIT)  return;
+  int v = digitalRead(R1_2);
+  if (DEBUG>1) Serial.println("++1");
+  if (millis() < lr11)  return;
   lr11 = millis();
 
-  if (digitalRead(R1_2 + INPUT_WAIT) == HIGH) {    
+  if (v == HIGH) {    
       rval[0]++;    
   } 
 }
 
 
 void do12() {
+  int v = digitalRead(R1_1);
+  if (DEBUG>1) Serial.println("--1");
   if (millis() < lr11 + INPUT_WAIT)  return;
   lr11 = millis();
 
-  if (digitalRead(R1_1) == HIGH) {    
+  if (v == HIGH) {    
       rval[0]--;    
   } 
 }
 // 2
 void do21() {  
+  int v = digitalRead(R2_2);
+  if (DEBUG>1) Serial.println("++2");
   if (millis() < lr21 + INPUT_WAIT)  return;  
   lr21 = millis();
 
-  if (digitalRead(R2_2) == HIGH) {    
+  if (v == HIGH) {    
       rval[1]++;    
   } 
 }
 void do22() {
-  
+  int v = digitalRead(R2_1);
+  if (DEBUG>1) Serial.println("--2");
   if (millis() < lr21 + INPUT_WAIT)  return;
   lr11 = millis();
 
-  if (digitalRead(R2_1) == HIGH) {    
+  if (v == HIGH) {    
       rval[1]--;    
   } 
 }
 
 // 3
 void do31() {
+  int v = digitalRead(R3_2);
+  if (DEBUG>1) Serial.println("++3");
   if (millis() < lr31 + INPUT_WAIT)  return;
   lr31 = millis();
 
-  if (digitalRead(R3_2 + INPUT_WAIT) == HIGH) {    
+  if (v == HIGH) {    
       rval[2]++;    
   } 
 }
 
 
 void do32() {
+  int v = digitalRead(R3_1);
+  if (DEBUG>1) Serial.println("--3");
   if (millis() < lr31 + INPUT_WAIT)  return;
   lr31 = millis();
 
-  if (digitalRead(R3_1) == HIGH) {    
+  if (v == HIGH) {    
       rval[2]--;    
   } 
 }
 
 // 4
 void do41() {
+  int v = digitalRead(R4_2);
   if (millis() < lr41 + INPUT_WAIT)  return;
   lr41 = millis();
 
-  if (digitalRead(R4_2 + INPUT_WAIT) == HIGH) {    
+  if (v == HIGH) {    
       rval[3]++;    
   } 
 }
 
 void do42() {
+  int v = digitalRead(R4_1);
   if (millis() < lr41 + INPUT_WAIT)  return;
   lr41 = millis();
 
-  if (digitalRead(R4_1) == HIGH) {    
+  if (v == HIGH) {    
       rval[3]--;    
   } 
 }
 
 // 5
 void do51() {
+  int v = digitalRead(R5_2);
   if (millis() < lr51 + INPUT_WAIT)  return;
   lr51 = millis();
 
-  if (digitalRead(R5_2 + INPUT_WAIT) == HIGH) {    
+  if (v == HIGH) {    
       rval[4]++;    
   } 
 }
 
 
 void do52() {
+  int v = digitalRead(R5_1);
   if (millis() < lr51 + INPUT_WAIT)  return;
   lr51 = millis();
 
-  if (digitalRead(R5_1) == HIGH) {    
+  if (v == HIGH) {    
       rval[4]--;    
   } 
 }
 
 // 6
 void do61() {
+  int v = digitalRead(R6_2);
   if (millis() < lr61 + INPUT_WAIT)  return;
   lr61 = millis();
 
-  if (digitalRead(R6_2 + INPUT_WAIT) == HIGH) {    
+  if (v == HIGH) {    
       rval[5]++;    
   } 
 }
 
 
 void do62() {
+  int v = digitalRead(R6_1);
   if (millis() < lr61 + INPUT_WAIT)  return;
   lr61 = millis();
 
-  if (digitalRead(R6_1) == HIGH) {    
+  if (v == HIGH) {    
       rval[0]--;    
   } 
 }
